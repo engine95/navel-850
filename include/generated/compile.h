674bdf3bbca27d174afd05c5e8fff9b4dcaad2e8
engine95 <mustangsroc@gmail.com>
2015-09-13 13:21:32 -0600

    Added IntelliThermal V2 and named Navel kernel

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 8 18:57:45 MDT 2015"
-#define LINUX_COMPILE_BY "engine95"
-#define LINUX_COMPILE_HOST "engine95-VirtualBox"
-#define LINUX_COMPILER "gcc version 4.8.2 20130719 (prerelease) (SM-Toolchain) "
